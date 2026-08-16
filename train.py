7f4904ed0f452a7665094598ea7072a7f405475d
Aatmaj Patro <aatmajpatro@gmail.com>
2026-08-16 15:12:38 +0530

    Refactor training process and evaluation in train.py

--- train.py
+++ train.py
@@ -1,10 +1,8 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 data = pd.read_csv("data/student.csv")
-
-print(data.head())
-print(data.shape)
-print(data.info())
 
 X = data[
     [
@@ -17,8 +15,24 @@ X = data[
 
 y = data["final_score"]
 
-print("Features:")
-print(X)
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
 
-print("\nTarget:")
-print(y)
+print("X_train:", X_train.shape)
+print("X_test:", X_test.shape)
+
+model = LinearRegression()
+
+model.fit(X_train, y_train)
+
+print("Weights:", model.coef_)
+print("Bias:", model.intercept_)
+
+predictions = model.predict(X_test)
+
+print("Predictions:", predictions)
+print("Actual:", y_test.values)
